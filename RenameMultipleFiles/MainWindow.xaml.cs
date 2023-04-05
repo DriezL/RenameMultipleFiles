f18b0afeb45d15c6db0bc1dbe5f2ceb654b3d510
Dries de Groot <dries.de.groot@pixelzebra.nl>
2023-04-05 12:45:43 +0200

    Initial commit

---
+++ RenameMultipleFiles/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace RenameMultipleFiles
+{
+    public partial class OpenFileDialogMultipleFilesSample : Window
+    {
+        public OpenFileDialogMultipleFilesSample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                Multiselect = true,
+                Filter = "Pdf files (*.pdf)|*.pdf|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                lbFiles.Items.Clear();
+                foreach (string filename in Renamer.Files.Rename(
+                            openFileDialog.FileNames,
+                            txtOldPart.Text,
+                            txtNewPart.Text))
+                {
+                    lbFiles.Items.Add(filename);
+                }
+            }
+        }
+    }
+}
---
+++ Renamer/RenameFiles.fs
@@ -0,0 +1,22 @@
+﻿namespace Renamer
+
+open System.IO
+open System
+
+module Files =
+    let Rename sequenceOfFiles oldpart newpart = 
+        let mutable doneFiles = []
+        for oldFile in sequenceOfFiles do
+            if not (File.Exists(oldFile)) then
+                let warning = $"Can not find {oldFile}" 
+                doneFiles <- [warning] |> List.append doneFiles ;
+            else
+                let newFile = oldFile.Replace(oldpart, newpart, StringComparison.OrdinalIgnoreCase)
+                let changed = newFile.CompareTo(oldFile) <> 0
+                if changed then
+                    System.IO.File.Move(oldFile, newFile)
+                    doneFiles <- [newFile] |> List.append doneFiles
+                else
+                    doneFiles <- [$"Part '{oldpart}' not found in file '{oldFile}'"] |> List.append doneFiles
+
+        doneFiles
